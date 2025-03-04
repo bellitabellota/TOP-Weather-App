db4448bb2e326896ffe1df90b73a4e5356f8358b
bellitabellota <136174454+bellitabellota@users.noreply.github.com>
2025-03-04 13:57:25 +0000

    Add 'loading' animation

--- src/index.js
+++ src/index.js
@@ -5,6 +5,11 @@ const inputElem = document.getElementById("location");
 const searchResultContainer = document.querySelector(".js-search-result");
 
 searchButton.addEventListener("click", () => {
+  searchResultContainer.innerHTML = `<div class="loading-container">
+      <div class="loading"></div>
+      <div class="loading"></div>
+      <div class="loading"></div>
+    </div>`;
   const getWeatherData = new Promise (function(resolve) {
     const searchedLocation = inputElem.value.toLowerCase().trim();
     fetchWeatherData(searchedLocation, resolve);
