d10aeb0f7da54e3d0f7703c49e64bcff02ddf97b
bellitabellota <136174454+bellitabellota@users.noreply.github.com>
2025-03-04 11:49:15 +0000

    Throw error if data could not be fetched and add error handler

--- src/index.js
+++ src/index.js
@@ -4,25 +4,32 @@ const searchButton = document.querySelector(".js-search-button");
 const inputElem = document.getElementById("location");
 const searchResultContainer = document.querySelector("js-search-result");
 
-let fetchedWeatherData = new Promise (function(resolve) {
+let getWeatherData = new Promise (function(resolve) {
   searchButton.addEventListener("click", () => {
     const searchedLocation = inputElem.value.toLowerCase().trim();
     fetchWeatherData(searchedLocation, resolve);
   })
 });
 
-  fetchedWeatherData.then(function(result){
+  getWeatherData.then(function(result){
     console.log(result);
   })
 
 function fetchWeatherData(location, resolve) {
   fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=BMP4FPLMSRRNBQRAKYHDX5L95`)
   .then(function(result){
-    return result.json();
+    if(!result.ok) {
+      throw new Error(`Response.status: ${result.status}`)
+    } else {
+      return result.json();
+    }  
   })
   .then(function(result) {
     const weatherData = {location, temperature: result.days[0].temp, description: result.days[0].description}
     resolve(weatherData);
+  },
+  function(error) {
+    console.log(`The data could not be fetched. ${error.message}`);
   })
 }
 
