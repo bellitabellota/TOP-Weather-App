8b414cc1fe8fe8171a439375ca0ea2a1d3e41a06
bellitabellota <136174454+bellitabellota@users.noreply.github.com>
2025-03-04 11:31:56 +0000

    Fetch Weather data and console.log it

--- src/index.js
+++ src/index.js
@@ -1,4 +1,30 @@
-import { helloWorld } from "./hello_world";
 import "./styles.css";
 
-console.log(helloWorld);
+const searchButton = document.querySelector(".js-search-button");
+const inputElem = document.getElementById("location");
+const searchResultContainer = document.querySelector("js-search-result");
+
+let fetchedWeatherData = new Promise (function(resolve) {
+  searchButton.addEventListener("click", () => {
+    const searchedLocation = inputElem.value.toLowerCase().trim();
+    fetchWeatherData(searchedLocation, resolve);
+  })
+});
+
+  fetchedWeatherData.then(function(result){
+    console.log(result);
+  })
+
+function fetchWeatherData(location, resolve) {
+  fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=BMP4FPLMSRRNBQRAKYHDX5L95`)
+  .then(function(result){
+    return result.json();
+  })
+  .then(function(result) {
+    const weatherData = {location, temperature: result.days[0].temp, description: result.days[0].description}
+    resolve(weatherData);
+  })
+}
+
+
+
